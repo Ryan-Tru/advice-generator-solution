3ee3e152973dfecf09124174effe62ab1a07559e
Ryan Truong <ryan.truong2304@gmail.com>
2022-04-13 19:23:58 +1000

    Finished JavaScript Side

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // API [Advice Slip API] (https://api.adviceslip.com)
+
+const adviceBtn = document.querySelector('.advice-btn');
+const adviceBody = document.querySelector('.advice-body');
+const adviceId = document.querySelector('.advice-id');
+
+adviceBtn.addEventListener('click', () => {
+    fetch('https://api.adviceslip.com/advice')
+        .then(res => res.json())
+        .then(d => {
+            adviceBody.innerText = `"${d['slip']['advice']}"`;
+            adviceId.innerText = `Advice #${d['slip']['id']}`;
+        });
+});
+
